e87f009282c1a641cc03b9e5e908141504d12991
Josh Apgar <josh@welldonemarketing.com>
2018-03-26 09:52:45 -0400

    CoreML App Finished

---
+++ vision-app/View/RoundedShadowView.swift
@@ -0,0 +1,20 @@
+//
+//  RoundedShadowView.swift
+//  vision-app
+//
+//  Created by Josh Apgar on 3/23/18.
+//  Copyright © 2018 Josh Apgar. All rights reserved.
+//
+
+import UIKit
+
+class RoundedShadowView: UIView {
+    
+    override func awakeFromNib() {
+        self.layer.shadowColor = #colorLiteral(red: 0.2126879096, green: 0.2239724994, blue: 0.265286684, alpha: 1)
+        self.layer.shadowRadius = 15
+        self.layer.shadowOpacity = 0.75
+        self.layer.cornerRadius = self.frame.height / 2
+    }
+
+}
